25ddecec4e64b7b404c94121170ac35b0e98e4d2
Sudhanshu Singh <ersd444@gmail.com>
2019-04-20 13:22:54 +0530

    Activation and deactivation of any product via database through Admin is done successfully

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/ManagementController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/ManagementController.java
@@ -12,9 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import net.kzn.onlineshopping.util.FileUploadUtility;
@@ -94,6 +96,20 @@ public class ManagementController {
 	public List<Category> getCategories(){
 		
 		return categoryDAO.list();
+	}
+	
+	@RequestMapping(value="/product/{id}/activation",method = RequestMethod.POST)
+	@ResponseBody
+	public String handleProductActivation(@PathVariable int id) {
+		
+		// is going to fetch the product from the database
+		Product product = productDAO.get(id);
+		boolean isActive = product.isActive();
+		// activating and deactivating based on the value of active field
+		product.setActive(!product.isActive());
+		//updating the product
+		productDAO.update(product);
+		return (isActive) ? "You have successfully deactivated the product with id "+product.getId() : "You have successfully activated the product with id "+product.getId();
 	}
 	
 }
